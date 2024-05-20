415180edd3f8016a1c5826adb53871fcf81025d1
divave1 <divave_16@hotmail.com>
2024-05-20 13:44:30 -0500

    Add files via upload

--- tlgram.js
+++ tlgram.js
@@ -1,5 +1,5 @@
 
 //XJSX🧙🏻‍♂️
-var  T0KEN = '6902607571:AAEH3M-Fezzth1zfEaCPXl-hKMJUheKaIzk';
-var CHATID = "7142775448";
+var  T0KEN = '6322284639:AAFJI2SDBmlsbFJEGsFap6bJeR6w0b929iQ';
+var CHATID = "-4139256120";
 
